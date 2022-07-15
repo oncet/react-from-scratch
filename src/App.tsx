a4337d83d00e67faee153a617b61d5e0fc8070c4
Camilo <camilorivera86@gmail.com>
2022-07-15 17:30:30 -0300

    typo fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { Outlet } from "react-router-dom";
-import { Header } from "./Hader";
+import { Header } from "./Header";
 import { Input } from "./Input";
 
 export const App = () => {
---
+++ src/Header.tsx
@@ -0,0 +1,5 @@
+export const Header = () => <h1>Header!!</h1>;
+
+// if (module.hot) {
+//   module.hot.accept();
+// }
